48d24291734297375507e665fd7ae567bcdd1534
Saaz Ramsubhag <ramsubhag.saaz@gmail.com>
2026-03-09 18:29:01 -0400

    removed use of ghpages and added github actions for building nextjs app

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+const path = require('path')
+module.exports = {
+  turbopack: {
+    root: path.join(__dirname, '..'),
+  },
+}
+
 export default nextConfig;
