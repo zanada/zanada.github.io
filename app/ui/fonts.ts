c9168af76ffac937ef31c41dff2f363e1d1f059a
Saaz Ramsubhag <ramsubhag.saaz@gmail.com>
2026-04-06 10:50:08 -0400

    using geist font instead

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,17 +1,14 @@
-import { Lexend } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 
-// export const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"]
-// });
-
-// export const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"]
-// });
-
-export const lexend = Lexend({
-  variable: "--font-lexend",
+export const geistSans = Geist({
+  variable: "--font-geist-sans",
   subsets: ["latin"],
-  preload: false,
+  preload: false
 });
+
+export const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+  preload: false
+});
+
